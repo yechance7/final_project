118e2ad36cdbc44820ac1d5918a28ba21403fb15
yechance7 <yechance7@gmail.com>
2024-03-18 03:03:05 +0900

    update selenium

--- streamlit_demo.py
+++ streamlit_demo.py
@@ -65,7 +65,7 @@ def crawl_image_store(url):
     image = Image.open(BytesIO(img_data))
 
     # 브라우저 닫기
-    #driver.quit()
+    driver.quit()
 
     return image
 
@@ -120,20 +120,15 @@ def main(stores_data, item_data):
     st.title("CTEE")
     st.write("환영합니다! 디지털 콘텐츠 판매를 쉽고 빠르게 크티 플레이스")
 
-    # 페이지 내용 추가
     st.header("검색")
-
-    # 사용자 입력 받기
     user_input = st.text_input("찾고 계신 크리에이터 또는 콘텐츠가 있나요?", "")
 
     if st.button("검색"):
         st.write("최근 검색어:", user_input)
 
-        # 사용자 입력이 없는 경우 처리
         if not user_input:
             st.write("검색어를 입력하세요.")
         else: 
-            # 스토어 검색 및 결과 출력
             matching_stores = search_stores(stores_data, user_input)
             if matching_stores:
                 st.write(f"스토어에서 찾은 아이템: {len(matching_stores)}")
