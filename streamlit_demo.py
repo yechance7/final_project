6a2edfaccb77269853dc09f1098fa91b4c47f15a
yechance7 <yechance7@gmail.com>
2024-03-18 16:53:55 +0900

    update selenium

--- streamlit_demo.py
+++ streamlit_demo.py
@@ -2,11 +2,15 @@ import json
 import streamlit as st
 
 from selenium import webdriver
-#from selenium.webdriver.chrome.service import Service
+
 from selenium.webdriver.common.by import By
-#from webdriver_manager.chrome import ChromeDriverManager
-import shutil
 from selenium.webdriver.chrome.options import Options
+#from webdriver_manager.chrome import ChromeDriverManager
+#from selenium.webdriver.chrome.service import Service
+import shutil
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 from PIL import Image
 from io import BytesIO
@@ -44,7 +48,9 @@ def crawl_image_store(url):
     driver.get(url)
 
     # 이미지 태그 찾기
-    img_element = driver.find_element(By.CLASS_NAME, 'profile_img')
+    wait = WebDriverWait(driver, 10)  # 최대 10초까지 기다림
+    img_element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'profile_img')))
+    #img_element = driver.find_element(By.CLASS_NAME, 'profile_img')
 
     # 이미지 데이터 가져오기
     img_url = img_element.get_attribute('src')
