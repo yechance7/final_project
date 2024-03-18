12931f7856cfc5bb793087f3c7dc6a33de2fe355
yechance7 <yechance7@gmail.com>
2024-03-18 16:01:40 +0900

    update selenium

--- streamlit_demo.py
+++ streamlit_demo.py
@@ -88,7 +88,7 @@ def search_stores(data, user_input):
     matching_items = []
     for item in data:
         if user_input in item["title"] or user_input in item["content"]:
-            matching_items.append({"id": item["id"], "title": item["title"], "content": item["content"],"view_count": item["view_count"],"alias": item["alias"]})
+            matching_items.append({"id": item["id"], "title": item["title"], "content": item["content"],"view_count": item["view_count"],"alias": item['alias']})
     return sorted(matching_items, key=lambda x: x['view_count'], reverse=True)
 
 def search_items(data, user_input):
@@ -119,8 +119,9 @@ def main(stores_data, item_data):
             if matching_stores:
                 st.write(f"스토어에서 찾은 아이템: {len(matching_stores)}")
                 for store in matching_stores:
+                    st.write(f"스토어 id: {store['id']}, title: {store['title']}, content: {store['content'],"alias": item['alias']}")
                     url = f"https://ctee.kr/place/{store['alias']}"
-                    #st.write(f"스토어 id: {store['id']}, title: {store['title']}, content: {store['content']}")
+                
                     st.image(crawl_image_store(url), caption=f"스토어 id: {store['id']}, title: {store['title']}, content: {store['content']}", use_column_width=True)
             else:
                 st.write("스토어 검색 결과가 없습니다.")
