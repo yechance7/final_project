7818fb2bdcebd5e2affb815d6ce20c86b913e804
yechance7 <yechance7@gmail.com>
2024-03-19 16:39:31 +0900

    update streamlit

--- streamlit_demo.py
+++ streamlit_demo.py
@@ -55,19 +55,21 @@ def crawl_image_store(url):
 
 
 def search_stores(data, user_input):
-    matching_items = []
+    matching_items = set()
     for item in data:
         if user_input in item["title"] or user_input in item["content"]:
-            matching_items.append({"id": item["id"], "title": item["title"], "content": item["content"],"view_count": item["view_count"],"alias": item['alias']})
+            matching_items.add({"id": item["id"], "title": item["title"], "content": item["content"],"view_count": item["view_count"],"alias": item['alias']})
+    matching_items=list(matching_items)
     return sorted(matching_items, key=lambda x: x['view_count'], reverse=True)
 
 def search_items(data, user_input):
-    matching_items = []
+    matching_items = set()
     for item in data:
         content = item.get("content", "")
         simple_contents = item.get("simple_contents", "")
         if user_input in content or (simple_contents and user_input in simple_contents):
-            matching_items.append({"id": item["id"], "content": item["content"], "simple_contents": item["simple_contents"],"view_count": item["view_count"]})
+            matching_items.add({"id": item["id"], "content": item["content"], "simple_contents": item["simple_contents"],"view_count": item["view_count"]})
+    matching_items=list(matching_items)
     return sorted(matching_items, key=lambda x: x['view_count'], reverse=True)
 
 
