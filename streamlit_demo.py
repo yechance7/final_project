17b15eaaab1ad297fe8d695218b72d9b2d3b5c01
yechance7 <yechance7@gmail.com>
2024-03-19 16:41:38 +0900

    update streamlit

--- streamlit_demo.py
+++ streamlit_demo.py
@@ -58,7 +58,7 @@ def search_stores(data, user_input):
     matching_items = set()
     for item in data:
         if user_input in item["title"] or user_input in item["content"]:
-            matching_items.add({"id": item["id"], "title": item["title"], "content": item["content"],"view_count": item["view_count"],"alias": item['alias']})
+            matching_items.update({"id": item["id"], "title": item["title"], "content": item["content"],"view_count": item["view_count"],"alias": item['alias']})
     matching_items=list(matching_items)
     return sorted(matching_items, key=lambda x: x['view_count'], reverse=True)
 
@@ -68,7 +68,7 @@ def search_items(data, user_input):
         content = item.get("content", "")
         simple_contents = item.get("simple_contents", "")
         if user_input in content or (simple_contents and user_input in simple_contents):
-            matching_items.add({"id": item["id"], "content": item["content"], "simple_contents": item["simple_contents"],"view_count": item["view_count"]})
+            matching_items.update({"id": item["id"], "content": item["content"], "simple_contents": item["simple_contents"],"view_count": item["view_count"]})
     matching_items=list(matching_items)
     return sorted(matching_items, key=lambda x: x['view_count'], reverse=True)
 
