42e10a77ffad63ffd756bd3f92851205b0f9b524
yechance7 <yechance7@gmail.com>
2024-03-25 12:30:29 +0900

    update streamlit

--- streamlit_demo.py
+++ streamlit_demo.py
@@ -6,7 +6,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
-
 #streamlit run .\streamlit_demo.py
 
 @st.cache
